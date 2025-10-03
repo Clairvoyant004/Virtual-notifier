3b51e651e528c1e48e3c69d777c9315ec205e2d3
zainab oguntona <oguntonazainab@gmail.com>
2025-10-03 22:01:12 +0100

    Updated changes

--- bet9ja_tracker/settings.py
+++ bet9ja_tracker/settings.py
@@ -118,8 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]   # tells Django where to look for static files
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
