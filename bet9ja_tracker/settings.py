ccf878faa7f08b729f08cf8da070dc16582b0d88
clairvoyant <ojooluwaseun1973@gmail.com>
2025-10-12 13:50:03 +0100

    changed db to postgresql

--- bet9ja_tracker/settings.py
+++ bet9ja_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +78,9 @@ WSGI_APPLICATION = 'bet9ja_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        dj_database_url.config(default=os.environ.get("DATABASE_URL"))
     }
 }
 
